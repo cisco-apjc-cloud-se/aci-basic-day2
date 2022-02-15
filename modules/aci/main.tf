5f166c5f2dd1547a3c9a749bc7d30001595b7a13
Richard <richwats@cisco.com>
2022-02-15 14:37:38 +1100

    revert to l2

--- modules/aci/main.tf
+++ modules/aci/main.tf
@@ -18,22 +18,5 @@ data "aci_vmm_domain" "vmm" {
 
 ### Load Physical Domain
 data "aci_physical_domain" "phys" {
-  name  = var.phys_name 
+  name  = var.phys_name
 }
-
-// ### Load Common Tenant
-// data "aci_tenant" "common" {
-//   name  = "common"
-// }
-//
-// ### Load Common L3Out to Lab
-// data "aci_l3_outside" "lab" {
-//   tenant_dn   = data.aci_tenant.common.id
-//   name        = "Lab-9348-Peering"
-// }
-//
-// ### Load Common:Lab VRF
-// data "aci_vrf" "tf-common-lab" {
-//   tenant_dn   = data.aci_tenant.common.id
-//   name        = "Lab"
-// }
