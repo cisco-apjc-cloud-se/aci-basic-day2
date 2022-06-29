226fb28153c94497ec362a5b0a1d325898ec594f
Richard <richwats@cisco.com>
2022-06-29 11:54:38 +1000

    no outputs

--- outputs.tf
+++ outputs.tf
@@ -2,19 +2,19 @@
 #   value = module.aci.dpgs
 #   sensitive = false
 # }
-
-output "tenant_map" {
-  value = module.aci_tenants.tenant_map
-}
-
-output "vrf_map" {
-  value = module.aci_tenants.vrf_map
-}
-
-output "bd_map" {
-  value = module.aci_tenants.bd_map
-}
-
-output "ap_map" {
-  value = module.aci_tenants.ap_map
-}
+# 
+# output "tenant_map" {
+#   value = module.aci_tenants.tenant_map
+# }
+#
+# output "vrf_map" {
+#   value = module.aci_tenants.vrf_map
+# }
+#
+# output "bd_map" {
+#   value = module.aci_tenants.bd_map
+# }
+#
+# output "ap_map" {
+#   value = module.aci_tenants.ap_map
+# }
